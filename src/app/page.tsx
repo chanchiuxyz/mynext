5f67970e4229a8191f26afd917d5b7d20d6fa630
chanchiuxyz <chanchiuxyz@gmail.com>
2024-07-23 17:42:22 -0700

    api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  throw new Error("Page Error")
+  // throw new Error("Page Error")
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
