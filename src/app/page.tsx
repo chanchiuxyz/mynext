7a36a79645a6490e445e78912f4420d72cc5aea0
chanchiuxyz <chanchiuxyz@gmail.com>
2024-08-02 08:03:09 -0700

    middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
   // throw new Error("Page Error")
+  const data = await fetch('https://api.github.com/repos/vercel/next.js')
+  console.log(data)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
